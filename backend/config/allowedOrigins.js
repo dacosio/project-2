08f99e1bcada9b77bd97e69881255a8184e4b52a
dacosio <dacosio@gmail.com>
2023-08-23 18:20:53 -0700

    env

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://project-2-fe.onrender.com",
+  "https://project-2.onrender.com",
 ];
 
 module.exports = allowedOrigins;
