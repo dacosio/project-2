42a64ef00c71433ffc14c00541b40b8afa51c2da
dacosio <dacosio@gmail.com>
2023-08-23 17:16:48 -0700

    morgan

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,8 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3500;
-// app.use(morgan("combined"));
 connectDB();
 app.use(logger);
 app.use(cors(corsOptions));
