8f754b5b9c437a8ba26df6f1253acf50dcc58030
dacosio <dacosio@gmail.com>
2023-08-23 18:05:31 -0700

    fix 1

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,8 @@
 // add the react url to allowed origins after deployment
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "https://project-2-fe.onrender.com",
+];
 
 module.exports = allowedOrigins;
