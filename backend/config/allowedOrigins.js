614cc14a59bd689f1c42291dcc17977fb43f4d7d
dacosio <dacosio@gmail.com>
2023-08-23 18:34:13 -0700

    new allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://project-2-dacosio.vercel.app",
+  "https://project-2-app.vercel.app",
 ];
 
 module.exports = allowedOrigins;
