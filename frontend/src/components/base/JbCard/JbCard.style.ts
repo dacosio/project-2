ef6a8ae20e1e2d4ea37d10d13393c0e3b5b0db3e
jbhern-34 <134764513+jbhern-34@users.noreply.github.com>
2023-09-25 10:45:09 -0700

    Fixed card

--- frontend/src/components/base/JbCard/JbCard.style.ts
+++ frontend/src/components/base/JbCard/JbCard.style.ts
@@ -1,11 +1,30 @@
 import styled from "@emotion/styled";
 import { theme } from "../../../utils/Theme";
 
-export const Container = styled.button`
-  width: 100%;
-  height: 100%;
-  cursor: pointer;
-  background-color: #222;
-  transition: 0.3s;
-  border-radius: 100px;
+export const Container = styled.button``;
+
+export const Button = styled.button`
+text-transform: uppercase;
+font-weight: 700;
+font-size: .8rem;
+color: #912cf5;
+letter-spacing: 1px;
+background-color: unset;
+border: none;
+`;
+
+export const Card = styled.div`
+max-width: 300px;
+padding: 2em;
+display: grid;
+grid-gap: 1em;
+border-radius: 12px;
+background-color: white;
+box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.292);
+`;
+
+export const ButtonsDiv = styled.div`
+display: flex;
+gap: 1em;
+justify-content: flex-end;
 `;
--- frontend/src/components/base/JbCard/JbCard.view.tsx
+++ frontend/src/components/base/JbCard/JbCard.view.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { JbCardProps } from './JbCard.props';
-import { Container } from './JbCard.style';
+import { Container, Button, Card, ButtonsDiv } from './JbCard.style';
 
 const JbCard = (props: JbCardProps): JSX.Element => {
 
 
   return <Container> JbCard
 
-    <div className="hello"></div>
-    <div className="card">
+    <Card>
       <div className="cardContent">
         <h3>Subtitle 1</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dolorem reiciendis ab rem sunt architecto officia nemo. Consequuntur commodi tenetur beatae iure, aut placeat.</p>
       </div>
-      <div className="buttonsDiv">
-        <button className="buttonDefault">Button</button>
-        <button className="buttonDefault">Button</button>
-      </div>
-    </div>
+      <ButtonsDiv>
+        <Button>Hello</Button>
+        <Button>Hello</Button>
+      </ButtonsDiv>
+    </Card>
 
     <div className="checkbox">
       <input type="checkbox" className="checkbox" />
     </div>
 
-  </Container>;
+  </Container >;
+
+
 };
 
 export default React.memo(JbCard);
