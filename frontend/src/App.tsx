60be9bd3ee319a831045b927e18033ff234b7182
guiachavezpd <guia.chavez@peakdesign.com>
2023-09-26 20:47:37 -0700

    add route to my page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Auth/Login";
 import Home from "./pages/Home";
 import Private from "./pages/Private";
 import PersistAuth from "components/layout/PersistAuth";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="testpage" element={<TestPage />} />
         <Route element={<PersistAuth />}>
           <Route path="private" element={<Private />} />
         </Route>
