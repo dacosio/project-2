5772ac0d42abd998c60b11a71725a5a8ee3b1ffa
guiachavezpd <guia.chavez@peakdesign.com>
2023-10-03 14:53:13 -0700

    remove other ports

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 // add the react url to allowed origins after deployment
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:3002",
   "http://127.0.0.1:3000",
   "https://project-2-app.vercel.app",
 ];
