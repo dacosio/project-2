8cca82c3061892f07f9c8130437957b05a4f91a1
guiachavezpd <guia.chavez@peakdesign.com>
2023-10-06 08:02:50 -0700

    update weather card

--- frontend/src/components/base/WeatherCard/WeatherCard.style.ts
+++ frontend/src/components/base/WeatherCard/WeatherCard.style.ts
@@ -1,26 +1,55 @@
 import styled from "@emotion/styled";
 import { theme } from "../../../utils/Theme";
-import { ImgHTMLAttributes } from "react";
 
 export const Container = styled.div`
     position: relative;
     width: 50%;
-    padding: 1.5rem;
+    max-width: 400px;
+    padding: 1.5rem 0.5rem 1.5rem 1.5rem;
     background-color: ${theme.brand.secondary};
     border-radius: 1.5rem;
+
+    .right-side-content {
+        width: 45%;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem
+    }
+
+    .left-side-content {
+        width: 55%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+    }
+
+    span {
+        font-weight: 700;
+    }
+
+    .weather-content {
+        display: flex;
+    }
 `
 
-export const ContentContainer = styled.div`
-    display: flex;
-`
-
-export const DetailsContainer = styled.div`
+export const LocationContainer = styled.div`
 
 `
+export const TemperatureContainer = styled.div`
 
-export const ImageContainer = styled.img<ImgHTMLAttributes<HTMLImageElement>>`
+`
+
+export const ForecastContainer = styled.div`
+    padding-right: 1rem;
+`
+
+export const CloudContainer = styled.div`
     position: absolute;
     top: -3rem;
-    width: 250px;
     right: 0;
+
+    @media (min-width: 1024px) {
+        width: 17rem;
+        max-width: 250px;
+    }
 `
--- frontend/src/components/base/WeatherCard/WeatherCard.view.tsx
+++ frontend/src/components/base/WeatherCard/WeatherCard.view.tsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { WeatherCardProps } from './WeatherCard.props';
-import { Container, ContentContainer, DetailsContainer, ImageContainer } from './WeatherCard.style';
-import Typography from '../Typography'
-import clouds from './../../../assets/clouds.png'
+import {
+  Container,
+  LocationContainer,
+  TemperatureContainer,
+  ForecastContainer,
+  CloudContainer
+} from './WeatherCard.style';
+import Typography from '../Typography';
+import Clouds from '../SVG/Clouds';
+import Moment from 'react-moment';
 
 const WeatherCard = (props: WeatherCardProps): JSX.Element => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState(
+    {
+      highTemperature: '22°C',
+      lowTemperature: '18°C',
+      currentTemperature: '20°C',
+      humidity: '65%',
+      windSpeed: '18%',
+      precipitation: '0%',
+    }
+  );
+  const date = new Date();
 
   useEffect(() => {
     // get data from weather API and use the data on setWeatherData
@@ -13,32 +30,39 @@ const WeatherCard = (props: WeatherCardProps): JSX.Element => {
 
   return (
     <Container className='weather-card'>
-      <ContentContainer>
-        <div style={{ width: "45%" }}>
-          <DetailsContainer>
-            <Typography variant='body'>Vancouver, BC</Typography>
-            <Typography variant='small'>Sat, Dec 1, 2023</Typography>
-          </DetailsContainer>
-          <DetailsContainer>
-            <Typography variant='title5'>Partly Cloudy</Typography>
-            <Typography variant='title2'>20C</Typography>
-            <Typography variant='small'>High 22C Low 18C</Typography>
-          </DetailsContainer>
-        </div>
+      {weatherData ?
+        <div className="weather-content">
+          <div className="right-side-content">
+            <LocationContainer>
+              <Typography variant='body' weight='700'>Vancouver, BC</Typography>
+              <Typography variant='caption'>
+                <Moment format="ddd, MMM D, YYYY">{date}</Moment>
+              </Typography>
+            </LocationContainer>
+            <TemperatureContainer>
+              <Typography variant='title5' weight='700'>Partly Cloudy</Typography>
+              <Typography variant='title2' weight='900'>{weatherData.currentTemperature}</Typography>
+              <Typography variant='label'>High <span>{weatherData.highTemperature}</span> Low <span>{weatherData.lowTemperature}</span></Typography>
+            </TemperatureContainer>
+          </div>
 
-        <div style={{ width: "55%", display: "flex", alignItems: "flex-end" }}>
-          <DetailsContainer>
-            <ImageContainer src={clouds} alt='clouds' />
-          </DetailsContainer>
-          <DetailsContainer>
-            <Typography variant='small'>Precipitation</Typography>
-            <Typography variant='small'>Humidity</Typography>
-            <Typography variant='small'>Wind</Typography>
-          </DetailsContainer>
+          <div className='left-side-content'>
+            <CloudContainer>
+              {/* <ImageContainer src={Clouds} alt='clouds' /> */}
+              <Clouds fill='green' />
+            </CloudContainer>
+            <ForecastContainer>
+              <Typography variant='label' align='right'>Precipitation <span>{weatherData.precipitation}</span></Typography>
+              <Typography variant='label' align='right'>Humidity <span>{weatherData.humidity}</span></Typography>
+              <Typography variant='label' align='right'>Wind <span>{weatherData.windSpeed}</span></Typography>
+            </ForecastContainer>
+          </div>
         </div>
-      </ContentContainer>
+        :
+        <Typography>Loading weather data.</Typography>
+      }
     </Container>
-  )
+  );
 };
 
 export default React.memo(WeatherCard);
