c464678ec87e55ab4c1ed1a50d55f28e8ac3590c
jbhern-34 <134764513+jbhern-34@users.noreply.github.com>
2023-10-07 16:05:56 -0700

    Practice grid system

--- frontend/src/pages/Home/Home.style.ts
+++ frontend/src/pages/Home/Home.style.ts
@@ -1,49 +1,68 @@
 import styled from "@emotion/styled";
+import { Col } from "react-grid-system";
 import { theme } from "utils/Theme";
 
 export const Container = styled.div``;
 
 export const Header = styled.div`
-background-color: #FCFFEB;
-padding: 1rem;
-height: 100vh;
-display: flex;
-align-items: center;
-justify-content: center;
-.header-contents{
+  background-color: #fcffeb;
+  padding: 1rem;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .header-contents {
     max-width: 500px;
-}
-.input-landing-page{
+  }
+  .input-landing-page {
     display: flex;
     gap: 1rem;
-}
+  }
 `;
 
 export const About = styled.div`
-background-color: #FFFFFF;
-padding: 1rem;
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 6rem;
-height: 100vh;
-.about-header{
+  background-color: #ffffff;
+  padding: 1rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6rem;
+  height: 100vh;
+  .about-header {
     display: flex;
     flex-flow: column;
-}
+  }
+  .about-details {
+    display: grid;
+    gap: 2rem;
+  }
 `;
 
 export const Details = styled.div`
-
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  img {
+    max-width: 80px;
+    height: auto;
+  }
 `;
 
 export const Contact = styled.div`
-background-color: #57CB62;
-padding: 1rem;
-height: 100vh;
+  background-color: #57cb62;
+  padding: 1rem;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  img {
+    height: 70vh;
+  }
+`;
+export const Column = styled(Col)`
+  background-color: red;
 `;
 
-
-
-
-
+export const Sacolumn = styled(Col)`
+  background-color: white;
+`;
--- frontend/src/pages/Home/Home.view.tsx
+++ frontend/src/pages/Home/Home.view.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { HomeGeneratedProps } from "./Home.props";
-import { Container, Header, About, Details, Contact } from "./Home.style";
+import { Container, Header, About, Details, Contact, Column, Sacolumn } from "./Home.style";
 import Typography from "components/base/Typography";
 import TextField from "components/base/TextField";
 import Button from "components/base/Button";
 import headerImg from "images/headerImg.png";
 import aboutimg from "images/aboutimg.png";
+import icon1 from "images/icon1.png";
+import icon2 from "images/icon2.png";
+import icon3 from "images/icon3.png";
+import contact from "images/contact.png";
+import Footer from "components/layout/Footer";
 import axios from "axios";
+import { Row, Col } from "react-grid-system";
 
 const HomeView = (props: HomeGeneratedProps) => {
   const [email, setEmail] = useState<string>("");
@@ -14,6 +20,12 @@ const HomeView = (props: HomeGeneratedProps) => {
 
   return (
     <Container>
+      <Row>
+        <Column sm={4}>One of three columns</Column>
+        <Sacolumn sm={4}>One of three columns</Sacolumn>
+        <Column sm={4}>One of three columns</Column>
+      </Row>
+
       <Header>
         <div className="header-contents">
           <Typography variant="title1" weight="700" textColor="accent">
@@ -24,7 +36,7 @@ const HomeView = (props: HomeGeneratedProps) => {
               value={email}
               onChangeText={setEmail}
               placeholder="user@nomail.com"
-              style={{flexGrow: "1"}}
+              style={{ flexGrow: "1" }}
             />
             <Button
               text="Farm with us"
@@ -34,67 +46,102 @@ const HomeView = (props: HomeGeneratedProps) => {
             ></Button>
           </div>
         </div>
-        <img src={headerImg} alt="" style={{maxHeight:"70vh"}}/>
+        <img src={headerImg} alt="" style={{ maxHeight: "70vh" }} />
       </Header>
 
       <About>
         <div className="about-header">
-          <Typography variant="title1" weight="700" textColor="accent"
-          style={{position: "relative", top:"100px", zIndex:"1"}}
+          <Typography
+            variant="title1"
+            weight="700"
+            textColor="accent"
+            style={{ position: "relative", top: "150px", zIndex: "1" }}
           >
             Farming<br></br>simplified
           </Typography>
-          <img src={aboutimg} alt="" 
-          style={{maxHeight:"70vh", position: "relative", bottom:"100px"}}/>
+          <img
+            src={aboutimg}
+            alt=""
+            style={{
+              height: "auto",
+              width: "40vw",
+              position: "relative",
+              bottom: "150px",
+            }}
+          />
         </div>
 
         <div className="about-details">
           <Details>
-            <Typography variant="title2" weight="700" textColor="n90">
-              Get our tailor-made recs
-            </Typography>
-            <Typography variant="body" weight="400" textColor="n80">
-              Know which crops are best for your planting and weather conditions
-              and also get a yield estimate
-            </Typography>
+            <img src={icon1} alt="" />
+
+            <div>
+              <Typography variant="title2" weight="700" textColor="n90">
+                Get our tailor-made recs
+              </Typography>
+              <Typography variant="body" weight="400" textColor="n80">
+                Know which crops are best for your planting and weather
+                conditions and also get a yield estimate
+              </Typography>
+            </div>
           </Details>
           <Details>
-            <Typography variant="title2" weight="700" textColor="n90">
-              Know your crops
-            </Typography>
-            <Typography variant="body" weight="400" textColor="n80">
-              With our crop database, you’ll get the necessary info about your
-              crops and resources needed to grow ‘em
-            </Typography>
+            <img src={icon2} alt="" />
+            <div>
+              <Typography variant="title2" weight="700" textColor="n90">
+                Know your crops
+              </Typography>
+              <Typography variant="body" weight="400" textColor="n80">
+                With our crop database, you’ll get the necessary info about your
+                crops and resources needed to grow ‘em
+              </Typography>
+            </div>
           </Details>
           <Details>
-            <Typography variant="title2" weight="700" textColor="n90">
-              Be ready, rain or shine
-            </Typography>
-            <Typography variant="body" weight="400" textColor="n80">
-              Always stay ahead of any weather situation with our long-range
-              weather forecast
-            </Typography>
+            <img src={icon3} alt="" />
+            <div>
+              <Typography variant="title2" weight="700" textColor="n90">
+                Be ready, rain or shine
+              </Typography>
+              <Typography variant="body" weight="400" textColor="n80">
+                Always stay ahead of any weather situation with our long-range
+                weather forecast
+              </Typography>
+            </div>
           </Details>
         </div>
       </About>
       <Contact>
-        <Typography variant="title2" weight="700" textColor="n90">
-          We’d love to hear from you!
-        </Typography>
-        <TextField
-          label="Name"
-          value={name}
-          onChangeText={setName}
-          placeholder="Juan Dela Cruz"
-        />
-        <TextField
-          label="Email"
-          value={email}
-          onChangeText={setEmail}
-          placeholder="user@nomail.com"
-        />
+        <img src={contact} alt="" />
+        <div>
+          <Typography variant="title2" weight="700" textColor="n0">
+            We’d love to hear from you!
+          </Typography>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <TextField
+              label="Name"
+              value={name}
+              onChangeText={setName}
+              placeholder="Juan Dela Cruz"
+              style={{ flexGrow: "1" }}
+            />
+            <TextField
+              label="Email"
+              value={email}
+              onChangeText={setEmail}
+              placeholder="user@nomail.com"
+              style={{ flexGrow: "1" }}
+            />
+          </div>
+          <TextField
+            label="Comments"
+            value={email}
+            onChangeText={setEmail}
+            placeholder="user@nomail.com"
+          />
+        </div>
       </Contact>
+      <Footer today=""></Footer>
     </Container>
   );
 };
