a44629961152e4151396d0692d3e01692f0b5f30
jbhern-34 <134764513+jbhern-34@users.noreply.github.com>
2023-10-08 21:51:18 -0700

    Rewrite code part 2

--- frontend/src/pages/Home/Home.style.ts
+++ frontend/src/pages/Home/Home.style.ts
@@ -16,45 +16,45 @@ export const HeaderContents = styled(Col)`
 export const Hide = styled(Hidden)`
 `;
 
-export const About = styled.div`
-  background-color: #ffffff;
-  padding: 1rem 10rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 6rem;
-  height: 100vh;
-  .about-header {
-    display: flex;
-    flex-flow: column;
-  }
-  .about-details {
-    display: grid;
-    gap: 2rem;
-  }
-`;
+// export const About = styled.div`
+//   background-color: #ffffff;
+//   padding: 1rem 10rem;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 6rem;
+//   height: 100vh;
+//   .about-header {
+//     display: flex;
+//     flex-flow: column;
+//   }
+//   .about-details {
+//     display: grid;
+//     gap: 2rem;
+//   }
+// `;
 
-export const Details = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  img {
-    max-width: 80px;
-    height: auto;
-  }
-`;
+// export const Details = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 1rem;
+//   img {
+//     max-width: 80px;
+//     height: auto;
+//   }
+// `;
 
-export const Contact = styled.div`
-  background-color: #57cb62;
-  padding: 1rem;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-  img {
-    height: 70vh;
-  }
-`;
+// export const Contact = styled.div`
+//   background-color: #57cb62;
+//   padding: 1rem;
+//   height: 100vh;
+//   display: flex;
+//   align-items: center;
+//   gap: 2rem;
+//   img {
+//     height: 70vh;
+//   }
+// `;
 
 export const Column = styled(Col)`
   background-color: red;
