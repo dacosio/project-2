61f4cb6a78abaa0f9f3384f428096c83739672a1
guiachavezpd <guia.chavez@peakdesign.com>
2023-10-10 16:48:01 -0700

    remove CropCondition for now

--- frontend/.storybook/stories/base/CropCondition.stories.tsx
+++ frontend/.storybook/stories/base/CropCondition.stories.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import type { Meta, StoryObj } from "@storybook/react";
-import CropCondition from "../../../src/components/base/CropCondition";
 import Container from "../../components/Container";
 import Wrapper from "../../components/Wrapper";
 
-const meta: Meta<typeof CropCondition> = {
-  title: "Base/CropCondition",
-  component: () =>
-    <CropCondition title='Temperature' output='21-27°C' />
+const meta: Meta<typeof Container> = {
+  title: "Base/Container",
+  component: Container
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary = () => <CropCondition title='Temperature' output="25°C" />;
+export const Primary = () => <Container children={undefined} />;
