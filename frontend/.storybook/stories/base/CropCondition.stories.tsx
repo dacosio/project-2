71a391343972871bff5b258a5f454a75d0efe9ca
guiachavezpd <guia.chavez@peakdesign.com>
2023-10-10 18:20:50 -0700

    fixed storybook

--- frontend/.storybook/stories/base/CropCondition.stories.tsx
+++ frontend/.storybook/stories/base/CropCondition.stories.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import type { Meta, StoryObj } from "@storybook/react";
 import Container from "../../components/Container";
+import CropCondition from './../../../src/components/base/CropCondition'
 import Wrapper from "../../components/Wrapper";
 
-const meta: Meta<typeof Container> = {
-  title: "Base/Container",
-  component: Container
+const meta: Meta<typeof CropCondition> = {
+  title: "Base/CropCondition",
+  component: () => {
+    return (
+      <Container style={{ width: '265px', display: 'block' }}>
+        <CropCondition title='Temperature' output='12°C - 15°C' />
+      </Container>
+    )
+  }
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary = () => <Container children={undefined} />;
+export const Primary: Story = {};
