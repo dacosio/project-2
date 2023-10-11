60e7257236db5c45112addce79669b8875ef32b3
laubana <45812484+laubana@users.noreply.github.com>
2023-10-10 21:30:04 -0700

    created

--- frontend/src/components/base/TextAreaField/TextAreaField.style.ts
+++ frontend/src/components/base/TextAreaField/TextAreaField.style.ts
@@ -5,9 +5,10 @@ import Typography from "../../base/Typography";
 
 const fontStyle = `
   font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 24px;  
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  font-family: "Inter";
 `;
 
 export const Container = styled.div`
@@ -18,14 +19,21 @@ export const Container = styled.div`
 export const TextArea = styled.textarea`
   ${fontStyle};
   width: 100%;
+  min-width: 300px;
   border-radius: 4px;
-  border: 1px solid black;
+  border: 1px solid ${({ theme }) => theme.grey.shade3};
   box-sizing: border-box;
-  padding: 4px 8px;
+  padding: 8px;
+  color: ${({ theme }) => theme.grey.shade9};
+  background-color: ${({ theme }) => theme.grey.noshade};
+  resize: none;
 
   :focus {
     outline: none;
+    box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.2);
   }
 `;
 
-export const Error = styled(Typography)``;
+export const Error = styled(Typography)`
+  padding: 0 8px;
+`;
