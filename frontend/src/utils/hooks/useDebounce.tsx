03c3e7ace1f86e327c8fce0332f4d2a7a342a534
guiachavezpd <guia.chavez@peakdesign.com>
2023-10-11 21:36:23 -0700

    Update useDebounce.tsx

--- frontend/src/utils/hooks/useDebounce.tsx
+++ frontend/src/utils/hooks/useDebounce.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 // this is used for search fields
 function useDebounce<T>(value: T, delay: number): T {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
     useEffect(() => {
         const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
         return () => {
