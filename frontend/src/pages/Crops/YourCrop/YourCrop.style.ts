1cf6956a3e112337d75e8daa9202aff8a867226f
yban <laubana@gmail.com>
2023-10-12 21:51:00 -0700

    created

--- frontend/src/pages/Crops/YourCrop/YourCrop.style.ts
+++ frontend/src/pages/Crops/YourCrop/YourCrop.style.ts
@@ -1,5 +1,64 @@
 import styled from "@emotion/styled";
+import { Col } from "react-grid-system";
 import { theme } from "utils/Theme";
 
+export const Container = styled.div`
+  display: grid;
+  margin: auto;
+  padding: 16px;
+`;
 
-export const Container = styled.div``;
+export const ListColumnContainer = styled(Col)`
+  display: grid;
+  gap: 16px;
+  width: 100%;
+  max-width: 500px;
+`;
+
+export const ColumnHeaderContainer = styled.div`
+  height: 50px;
+  display: grid;
+  align-items: center;
+`;
+
+export const ColumnBodyContainer = styled.div`
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  background-color: white;
+  border-radius: 20px;
+  padding: 32px 20px;
+  display: grid;
+  gap: 16px;
+`;
+
+export const CropItemContainer = styled.div`
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  padding: 20px 28px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ButtonContainer = styled.div`
+  display: grid;
+  justify-items: end;
+`;
+
+export const DetailColumnContainer = styled(Col)`
+  display: grid;
+  gap: 16px;
+  width: 100%;
+  max-width: 500px;
+  padding-top: 66px;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+`;
+
+export const InformationContainer = styled.div``;
+
+export const FertilizerContainer = styled.div``;
+
+export const SuggestionContainer = styled.div``;
+
+export const TipContainer = styled.div``;
--- frontend/src/pages/Crops/YourCrop/YourCrop.view.tsx
+++ frontend/src/pages/Crops/YourCrop/YourCrop.view.tsx
@@ -1,9 +1,96 @@
-import React from 'react';
-import { YourCropGeneratedProps } from './YourCrop.props';
-import { Container } from './YourCrop.style';
+import React from "react";
+import { YourCropGeneratedProps } from "./YourCrop.props";
+import {
+  Container,
+  CropItemContainer,
+  ColumnBodyContainer,
+  ListColumnContainer,
+  ButtonContainer,
+  ColumnHeaderContainer,
+  DescriptionContainer,
+  DetailColumnContainer,
+  InformationContainer,
+  FertilizerContainer,
+  SuggestionContainer,
+  TipContainer,
+} from "./YourCrop.style";
+import Typography from "components/base/Typography";
+import { Row } from "react-grid-system";
+import theme from "utils/Theme";
+import Button from "components/base/Button";
 
 const YourCropView = (props: YourCropGeneratedProps) => {
-  return <Container> YourCrop </Container>;
+  return (
+    <Container>
+      <Row
+        nogutter
+        style={{
+          width: "100%",
+          gap: "16px",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
+      >
+        <ListColumnContainer
+          sm={6}
+          style={{ width: undefined, maxWidth: undefined }}
+        >
+          <ColumnHeaderContainer>
+            <Typography variant="title2">Your Crops</Typography>
+          </ColumnHeaderContainer>
+          <ColumnBodyContainer>
+            <CropItemContainer>
+              <Typography variant="body" weight="700">
+                Crop Name
+              </Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <CropItemContainer>
+              <Typography variant="body">Crop Name</Typography>
+              <Typography variant="body">Planted</Typography>
+            </CropItemContainer>
+            <Button text="New Crop" style={{ justifySelf: "end" }} />
+          </ColumnBodyContainer>
+        </ListColumnContainer>
+        <DetailColumnContainer
+          sm={6}
+          style={{ width: undefined, maxWidth: undefined }}
+        >
+          <ColumnBodyContainer>
+            <DescriptionContainer>h</DescriptionContainer>
+            <InformationContainer>wef</InformationContainer>
+            <FertilizerContainer>ewf</FertilizerContainer>
+            <SuggestionContainer>e</SuggestionContainer>
+            <TipContainer>w</TipContainer>
+          </ColumnBodyContainer>
+        </DetailColumnContainer>
+      </Row>
+    </Container>
+  );
 };
 
 export default YourCropView;
