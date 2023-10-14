a862fdf62fe4419b23edaef22b41d9e453971e1f
dacosio <dacosio@gmail.com>
2023-10-13 21:21:09 -0700

    init2

--- frontend/src/components/base/SegmentedControl/SegmentedControl.style.ts
+++ frontend/src/components/base/SegmentedControl/SegmentedControl.style.ts
@@ -8,10 +8,7 @@ export const Container = styled.div`
   width: 100%;
   // box-shadow: 0px 12px 24px rgba(41, 43, 50, 0.25);
   box-shadow: 0px 6px 12px rgba(41, 43, 50, 0.12);
-  background: ${(props) =>
-    props.theme.appType === 'buyer'
-      ? props.theme.grey.noshade
-      : props.theme.grey.shade9};
+  background: ${(props) => props.theme.grey.shade9};
   border-radius: 4px;
 
   display: inline-flex;
@@ -29,13 +26,9 @@ const ControlButtonColor: Record<'buyer' | 'seller', string> = {
 
 const ControlButtonTextColor = (
   active: boolean,
-  appType: 'buyer' | 'seller'
 ): string => {
-  if (appType === 'buyer') {
-    return active ? theme.grey.noshade : theme.grey.shade8;
-  } else {
+
     return active ? theme.grey.shade9 : theme.grey.noshade;
-  }
 };
 
 export const ControlButton = styled.button<{ active: boolean }>`
@@ -44,13 +37,12 @@ export const ControlButton = styled.button<{ active: boolean }>`
   width: 100%;
   border-radius: 4px;
   border: none;
-  background: ${(props) =>
-    props.active ? ControlButtonColor[props.theme.appType] : 'none'};
+  background: ${(props) => (props.active ? props.theme.grey.shade9 : "none")};
 
   /* Text Properties */
   font-size: 14px;
   color: ${(props) =>
-    ControlButtonTextColor(props.active, props.theme.appType)};
+    ControlButtonTextColor(props.active)};
   font-weight: 500;
   line-height: 24px;
 
