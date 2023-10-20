d1ab8416e95dfc9632d9c9955b0af6dd0ccf1883
laubana <45812484+laubana@users.noreply.github.com>
2023-10-19 22:47:31 -0700

    style changed

--- frontend/.storybook/stories/base/Accordion.stories.tsx
+++ frontend/.storybook/stories/base/Accordion.stories.tsx
@@ -9,7 +9,14 @@ const meta: Meta<typeof Accordion> = {
   component: () => {
     return (
       <Container>
-        <Accordion label="Accordion">t</Accordion>
+        <Accordion label="Accordion">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          aliquam ac neque eget accumsan. Aenean commodo est sed pharetra
+          facilisis. Donec ligula nunc, consectetur a ante ac, convallis blandit
+          tortor. Nulla nec lectus erat. Nam sodales vestibulum viverra. Donec
+          accumsan cursus accumsan. Aliquam nec posuere quam, id tincidunt dui.
+          Etiam dictum mollis mauris vitae scelerisque.
+        </Accordion>
       </Container>
     );
   },
