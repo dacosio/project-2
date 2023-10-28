073b2dbca7eee8b9ef79d66ffb1c4e77ab8d8491
yban <laubana@gmail.com>
2023-10-28 15:05:46 -0700

    created

---
+++ frontend/src/components/base/NumberField/NumberField.style.ts
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import { theme } from "../../../utils/Theme";
+import Typography from "../Typography";
+
+const fontStyle = `
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  font-family: "Inter";
+`;
+
+export const Container = styled.div`
+  display: grid;
+  gap: 8px;
+`;
+
+export const Number = styled.textarea`
+  ${fontStyle};
+  width: 100%;
+  min-width: 300px;
+  border-radius: 4px;
+  border: none;
+  box-sizing: border-box;
+  padding: 8px;
+  color: ${({ theme }) => theme.btn.text.primary};
+  background-color: ${({ theme }) => theme.bg.primary};
+  resize: none;
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.25);
+
+  ::placeholder {
+    color: ${({ theme }) => theme.btn.text.disabledToken};
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const Error = styled(Typography)`
+  padding: 0 8px;
+`;
---
+++ frontend/src/components/base/NumberField/NumberField.view.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NumberFieldProps } from './NumberField.props';
+import { Container } from './NumberField.style';
+
+const NumberField = (props: NumberFieldProps): JSX.Element => {
+  return <Container> NumberField </Container>;
+};
+
+export default React.memo(NumberField);
