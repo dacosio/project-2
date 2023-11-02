70fffa52e69d7ba54e5fbb79f13ae2501cfe83e0
guiachavezpd <guia.chavez@peakdesign.com>
2023-11-01 23:16:34 -0700

    Revert "update styling of Loading component"
    This reverts commit c50a419ddc79e354cfdfea9b6c1178f229747d53.

--- frontend/src/components/base/Loading/Loading.style.ts
+++ frontend/src/components/base/Loading/Loading.style.ts
@@ -1,22 +1,7 @@
 import styled from "@emotion/styled";
 import { theme } from "../../../utils/Theme";
 
-
-export const Wrapper = styled.div `
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.6);
-  cursor: pointer;
-  z-index: 9999;
-`
 export const Container = styled.div<{ width?: string; height?: string }>`
   height: ${({ height }) => (height ? height : "8rem")};
   width: ${({ width }) => (width ? width : "8rem")};
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 `;
--- frontend/src/components/base/Loading/Loading.view.tsx
+++ frontend/src/components/base/Loading/Loading.view.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { LoadingProps } from "./Loading.props";
-import { Container, Wrapper } from "./Loading.style";
+import { Container } from "./Loading.style";
 import Lottie from "lottie-react";
 import * as animation from ".././../../const/loading.json";
 
 const Loading = (props: LoadingProps): JSX.Element => {
   return (
-    <Wrapper>
-      <Container {...props}>
-        <Lottie animationData={animation} loop />
-      </Container>
-    </Wrapper>
+    <Container {...props}>
+      <Lottie animationData={animation} loop />
+    </Container>
   );
 };
 
