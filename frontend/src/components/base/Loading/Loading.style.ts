2399baea9030adf5b78459ca969c294a207d3136
laubana <45812484+laubana@users.noreply.github.com>
2023-11-11 13:18:03 -0800

    overlay changed

--- frontend/src/components/base/Loading/Loading.style.ts
+++ frontend/src/components/base/Loading/Loading.style.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.2); /* Overlay background color */
+  background-color: rgba(0, 0, 0, 0.1); /* Overlay background color */
   display: flex;
   justify-content: center;
   align-items: center;
