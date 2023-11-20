9ecd3ab8c508509a94f932e0f749575920a0a8eb
Don Cosio <dacosio@gmail.com>
2023-11-19 18:06:39 -0800

    allow oriings

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://project-2-app.vercel.app",
-  "https://www.sproutfarming.net/",
+  "https://www.sproutfarming.net",
 ];
 
 module.exports = allowedOrigins;
